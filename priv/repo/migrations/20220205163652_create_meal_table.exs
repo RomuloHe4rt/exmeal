24608fe5ddcdc28223162d0b8a311bfd36f5eab4
Rômulo Silva <romulo_lss@hotmail.com>
2022-02-05 14:04:01 -0300

    add schema

--- priv/repo/migrations/20220205163652_create_meal_table.exs
+++ priv/repo/migrations/20220205163652_create_meal_table.exs
@@ -2,5 +2,12 @@ defmodule Exmeal.Repo.Migrations.CreateMealTable do
   use Ecto.Migration
 
   def change do
+    create table(:foods) do
+      add :description, :string
+      add :date, :string
+      add :calories, :integer
+
+      timestamps()
+    end
   end
 end
